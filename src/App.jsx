1237b26c3a36f99ce9ddd487c347fa6bbef89226
junganthonyy <junganthonyy@gmail.com>
2024-10-02 16:26:41 -0700

    Adding TabButton Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 
 function App() {
@@ -20,7 +21,15 @@ function App() {
             })()}
           </ul>
         </section>
-        
+        <section id='examples'>
+            <h2>Examples</h2>
+            <menu>
+              <TabButton>Components</TabButton>
+              <TabButton>JSX</TabButton>
+              <TabButton>Props</TabButton>
+              <TabButton>State</TabButton>
+            </menu>
+        </section>
       </main>
     </div>
   );
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,13 @@
+export default function TabButton({children}) {
+  function handleClick() {
+    
+  }
+
+  return (
+    <li>
+      <button
+        onClick={{}}
+      >{children}</button>
+    </li>
+  )
+}
