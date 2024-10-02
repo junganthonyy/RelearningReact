5a3cadbe4cbf3bcbf8310e33ae7ece951ad5d1de
junganthonyy <junganthonyy@gmail.com>
2024-10-02 07:38:32 -0700

    Moving Components Into components Folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import reactImg from './assets/react-core-concepts.png'
-import componentsImg from './assets/components.png'
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.img} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.descrption}</p>
-    </li>
-  )
-}
 
 function App() {
   return (
@@ -38,14 +11,13 @@ function App() {
         <section id='core-concepts'>
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept 
-              title="Components"
-              descrption="The Core UI building block" 
-              img={componentsImg}
-            />
-            <CoreConcept/>
-            <CoreConcept/>
-            <CoreConcept/>
+            {(() => {
+              return CORE_CONCEPTS.map( props => {
+                return (<CoreConcept
+                    {...props}
+                  />)
+              })
+            })()}
           </ul>
         </section>
         
