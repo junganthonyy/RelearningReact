0a4e85b771aa5448aba9fcf4360f0ac6879220ca
junganthonyy <junganthonyy@gmail.com>
2024-10-02 17:08:55 -0700

    Using State To Dynamicly Display Values

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { CORE_CONCEPTS } from './data';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 import { useState } from 'react';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
@@ -6,9 +6,7 @@ import TabButton from './components/TabButton.jsx';
 
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
-
-  let tabContent = 'click a button'
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   let handleSelect = (selectedButton) => {
     setSelectedTopic(selectedButton)
@@ -33,12 +31,20 @@ function App() {
         <section id='examples'>
             <h2>Examples</h2>
             <menu>
-              <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
-              <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
-              <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
-              <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
+              <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+              <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+              <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+              <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
             </menu>
-            {selectedTopic}
+            <div id='tab-content'>
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>
         </section>
       </main>
     </div>
